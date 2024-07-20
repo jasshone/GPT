dee277bd02775ae8b3dc24f0c91fee498dd97290
jasshone <jasshone@gmail.com>
2024-07-20 12:08:16 -0400

    Update run.py

--- run.py
+++ run.py
@@ -9,3 +9,27 @@ n_embd = 384
 n_head = 6 #each head is 64 dimensional
 n_layer = 6
 dropout = 0.2
+from .data import TextData
+from .model import LanguageModel
+data = TextData()
+m = LanguageModel(data.vocab_size)
+#used because converges faster in some situations
+optimizer = torch.optim.AdamW(m.parameters(), lr = 1e-3)
+
+def train():
+  for steps in range(max_iters):
+      #get train data using our batch function
+      xb, yb = data.get_batch('train')
+      #eval loss and update model
+      logits, loss = m(xb, yb)
+      optimizer.zero_grad(set_to_none = True)
+      loss.backward()
+      optimizer.step()
+      if steps%500 == 0:
+        with model.eval():
+          xb, yb = data.get_batch('val')
+          logits, loss = m(xb, yb)
+          print(f"step {steps} loss: {loss}")
+          
+
+print(decode(m.generate(idx = torch.zeros((1,1), dtype = torch.log), max_new_tokens = 500)[0].tolist()))
